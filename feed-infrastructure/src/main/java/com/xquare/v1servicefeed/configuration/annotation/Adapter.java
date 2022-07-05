527a06bb155da42087190e13552d298259cf7285
정대현 <eogus_2513@dsm.hs.kr>
2022-07-05 13:38:06 +0900

    :recycle: :: @Adapter 어노테이션 bean 등록

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/annotation/Adapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/annotation/Adapter.java
@@ -1,10 +1,13 @@
 package com.xquare.v1servicefeed.configuration.annotation;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Component
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Adapter {
