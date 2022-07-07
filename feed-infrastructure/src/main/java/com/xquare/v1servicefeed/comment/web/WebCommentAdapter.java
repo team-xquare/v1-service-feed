1dbfb1f0a3bcaa8afa0e32a0202c205ddccfb66b
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 19:49:34 +0900

    ⚡️ :: createComment api 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
@@ -1,9 +1,33 @@
 package com.xquare.v1servicefeed.comment.web;
 
+import com.xquare.v1servicefeed.comment.api.CreateCommentApi;
+import com.xquare.v1servicefeed.comment.api.dto.request.DomainCreateCommnetRequest;
+import com.xquare.v1servicefeed.comment.web.dto.request.WebCreateCommentRequest;
+import com.xquare.v1servicefeed.configuration.security.SecurityAdapter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/comments")
 @RestController
 public class WebCommentAdapter {
+
+    private final SecurityAdapter securityAdapter;
+
+    private final CreateCommentApi createCommentApi;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createComment(WebCreateCommentRequest request) {
+        createCommentApi.execute(
+                DomainCreateCommnetRequest.builder()
+                        .userId(securityAdapter.getCurrentUserId())
+                        .feedUuid(request.getFeedUuid())
+                        .content(request.getContent())
+                        .build()
+        );
+    }
+
 }
