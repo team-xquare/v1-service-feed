d2e223f8f076a9cee04d6261d945e93e8b19769f
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 20:02:18 +0900

    :recycle: :: @PostMApping 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
@@ -6,6 +6,7 @@ import com.xquare.v1servicefeed.comment.web.dto.request.WebCreateCommentRequest;
 import com.xquare.v1servicefeed.configuration.security.SecurityAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class WebCommentAdapter {
     private final CreateCommentApi createCommentApi;
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public void createComment(WebCreateCommentRequest request) {
         createCommentApi.execute(
                 DomainCreateCommnetRequest.builder()
