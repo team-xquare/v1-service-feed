41dad180af20e3a3a7e6db3f711f0a6d1aed272f
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 20:05:33 +0900

    :recycle: :: Spi 어노테이션 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/CommentSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/CommentSpi.java
@@ -1,4 +1,7 @@
 package com.xquare.v1servicefeed.comment.spi;
 
+import com.xquare.v1servicefeed.annotation.Spi;
+
+@Spi
 public interface CommentSpi extends CommandCommentSpi {
 }
--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
@@ -1,6 +1,8 @@
 package com.xquare.v1servicefeed.feed.spi;
 
+import com.xquare.v1servicefeed.annotation.Spi;
 import com.xquare.v1servicefeed.comment.spi.CommentQueryFeedApi;
 
+@Spi
 public interface FeedSpi extends CommentQueryFeedApi {
 }
