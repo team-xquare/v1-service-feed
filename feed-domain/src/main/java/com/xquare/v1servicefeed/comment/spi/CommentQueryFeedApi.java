b7495541e92ceb169909082869816105c9aff389
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 20:04:27 +0900

    :recycle: :: Api 어노테이션 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/CommentQueryFeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/CommentQueryFeedApi.java
@@ -1,9 +1,11 @@
 package com.xquare.v1servicefeed.comment.spi;
 
+import com.xquare.v1servicefeed.annotation.Spi;
 import com.xquare.v1servicefeed.feed.Feed;
 
 import java.util.UUID;
 
+@Spi
 public interface CommentQueryFeedApi {
     Feed queryFeedById(UUID feedId);
 }
