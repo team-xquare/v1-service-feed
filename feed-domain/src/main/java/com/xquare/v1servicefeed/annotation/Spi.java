0ea452bd4d2ec682274adff737ffb85d86259513
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 20:03:38 +0900

    :bookmark_tabs: :: Spi

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/annotation/Spi.java
@@ -0,0 +1,12 @@
+package com.xquare.v1servicefeed.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+public @interface Spi { }
