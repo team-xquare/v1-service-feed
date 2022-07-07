f46c4dce4568aa8bafcbe49186c0aae2343d80c7
jaemin <sonjm3518@dsm.hs.kr>
2022-07-07 13:16:34 +0900

    📑 :: (#9) SecurityConfig

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.xquare.v1servicefeed.configuration.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors().and()
+                .csrf().disable()
+                .formLogin().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+
+                .and()
+                .apply(new FilterConfig(objectMapper))
+
+                .and()
+                .build();
+    }
+}
