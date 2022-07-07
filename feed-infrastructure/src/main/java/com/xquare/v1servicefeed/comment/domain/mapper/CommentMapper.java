d5cc94daeea0bbbba711e106c49ad8ba07d904d4
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 19:47:10 +0900

    :bookmark_tabs: :: CommentMapper

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/domain/mapper/CommentMapper.java
@@ -0,0 +1,37 @@
+package com.xquare.v1servicefeed.comment.domain.mapper;
+
+import com.xquare.v1servicefeed.comment.Comment;
+import com.xquare.v1servicefeed.comment.domain.CommentEntity;
+import com.xquare.v1servicefeed.feed.domain.FeedEntity;
+import com.xquare.v1servicefeed.feed.domain.repository.FeedRepository;
+import com.xquare.v1servicefeed.feed.exception.FeedNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CommentMapper {
+
+    private final FeedRepository feedRepository;
+
+    public Comment entityToDomain(CommentEntity commentEntity) {
+
+        return Comment.builder()
+                .id(commentEntity.getId())
+                .content(commentEntity.getContent())
+                .feedId(commentEntity.getFeed().getId())
+                .userId(commentEntity.getUserId())
+                .build();
+    }
+
+    public CommentEntity domainToEntity(Comment comment) {
+        FeedEntity feedEntity = feedRepository.findById(comment.getFeedId())
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+
+        return CommentEntity.builder()
+                .id(comment.getId())
+                .content(comment.getContent())
+                .feed(feedEntity)
+                .build();
+    }
+}
