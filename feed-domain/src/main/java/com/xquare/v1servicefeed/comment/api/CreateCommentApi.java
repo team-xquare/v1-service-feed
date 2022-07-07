84eb199fba32b4b46bd42e0197c1e89951766f2e
정대현 <eogus_2513@dsm.hs.kr>
2022-07-07 19:44:48 +0900

    :bookmark_tabs: :: CreateCommentApi

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/api/CreateCommentApi.java
@@ -0,0 +1,7 @@
+package com.xquare.v1servicefeed.comment.api;
+
+import com.xquare.v1servicefeed.comment.api.dto.request.DomainCreateCommnetRequest;
+
+public interface CreateCommentApi {
+    void execute(DomainCreateCommnetRequest request);
+}
