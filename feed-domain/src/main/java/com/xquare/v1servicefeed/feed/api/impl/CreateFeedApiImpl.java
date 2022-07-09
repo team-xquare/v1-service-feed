5712b0a53643643937cdd0b39bce383261999043
jaemin <sonjm3518@dsm.hs.kr>
2022-07-09 22:13:44 +0900

    ⚡ :: UUID → Feed로 반환하기

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/CreateFeedApiImpl.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/CreateFeedApiImpl.java
@@ -7,8 +7,6 @@ import com.xquare.v1servicefeed.feed.api.dto.DomainCreateFeedRequest;
 import com.xquare.v1servicefeed.feed.spi.CommandFeedSpi;
 import lombok.RequiredArgsConstructor;
 
-import java.util.UUID;
-
 @RequiredArgsConstructor
 @DomainService
 public class CreateFeedApiImpl implements CreateFeedApi {
@@ -16,14 +14,13 @@ public class CreateFeedApiImpl implements CreateFeedApi {
     private final CommandFeedSpi commandFeedSpi;
 
     @Override
-    public UUID execute(DomainCreateFeedRequest request) {
-        Feed feed = commandFeedSpi.saveFeed(
+    public Feed execute(DomainCreateFeedRequest request) {
+
+        return commandFeedSpi.saveFeed(
                 Feed.builder()
                         .title(request.getTitle())
                         .content(request.getContent())
                         .category(request.getCategory())
                         .build());
-
-        return feed.getId();
     }
 }
