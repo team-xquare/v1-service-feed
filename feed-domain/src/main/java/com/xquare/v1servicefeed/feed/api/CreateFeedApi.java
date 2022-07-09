27ef0661bb223aa93cfa96136f7144a1be8b3380
jaemin <sonjm3518@dsm.hs.kr>
2022-07-09 22:13:06 +0900

    ⚡ :: UUID → Feed

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
@@ -1,11 +1,10 @@
 package com.xquare.v1servicefeed.feed.api;
 
 import com.xquare.v1servicefeed.annotation.Api;
+import com.xquare.v1servicefeed.feed.Feed;
 import com.xquare.v1servicefeed.feed.api.dto.DomainCreateFeedRequest;
-
-import java.util.UUID;
 
 @Api
 public interface CreateFeedApi {
-    UUID execute(DomainCreateFeedRequest request);
+    Feed execute(DomainCreateFeedRequest request);
 }
