9aac2380f0406e0a9be2b994cbe6b3354731b03d
jaemin <sonjm3518@dsm.hs.kr>
2022-07-10 18:14:59 +0900

    ⚡ :: DomainFeedUuidResponse 반환하기

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
@@ -1,10 +1,10 @@
 package com.xquare.v1servicefeed.feed.api;
 
 import com.xquare.v1servicefeed.annotation.Api;
-import com.xquare.v1servicefeed.feed.Feed;
-import com.xquare.v1servicefeed.feed.api.dto.DomainCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.api.dto.response.DomainFeedUuidResponse;
 
 @Api
 public interface CreateFeedApi {
-    Feed execute(DomainCreateFeedRequest request);
+    DomainFeedUuidResponse execute(DomainCreateFeedRequest request);
 }
