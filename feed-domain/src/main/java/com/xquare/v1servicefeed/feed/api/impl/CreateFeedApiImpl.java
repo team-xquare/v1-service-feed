441e75a47da79d91851cd19fa24334f2594097fe
jaemin <sonjm3518@dsm.hs.kr>
2022-07-10 18:18:52 +0900

    📑 :: DomainFeedUuidResponse 반환하기

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/CreateFeedApiImpl.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/CreateFeedApiImpl.java
@@ -3,7 +3,8 @@ package com.xquare.v1servicefeed.feed.api.impl;
 import com.xquare.v1servicefeed.annotation.DomainService;
 import com.xquare.v1servicefeed.feed.Feed;
 import com.xquare.v1servicefeed.feed.api.CreateFeedApi;
-import com.xquare.v1servicefeed.feed.api.dto.DomainCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.api.dto.response.DomainFeedUuidResponse;
 import com.xquare.v1servicefeed.feed.spi.CommandFeedSpi;
 import lombok.RequiredArgsConstructor;
 
@@ -14,13 +15,17 @@ public class CreateFeedApiImpl implements CreateFeedApi {
     private final CommandFeedSpi commandFeedSpi;
 
     @Override
-    public Feed execute(DomainCreateFeedRequest request) {
+    public DomainFeedUuidResponse execute(DomainCreateFeedRequest request) {
 
-        return commandFeedSpi.saveFeed(
+        Feed feed = commandFeedSpi.saveFeed(
                 Feed.builder()
                         .title(request.getTitle())
                         .content(request.getContent())
                         .category(request.getCategory())
                         .build());
+
+        return DomainFeedUuidResponse.builder()
+                .feedUuid(feed.getId())
+                .build();
     }
 }
