0404a88a6bfd616a5a1563c909d6c450c2f7d888
jaemin <sonjm3518@dsm.hs.kr>
2022-07-11 08:32:38 +0900

    ⚡ :: Response 통일

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/CreateFeedApi.java
@@ -2,9 +2,9 @@ package com.xquare.v1servicefeed.feed.api;
 
 import com.xquare.v1servicefeed.annotation.Api;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
-import com.xquare.v1servicefeed.feed.api.dto.response.DomainFeedUuidResponse;
+import com.xquare.v1servicefeed.feed.api.dto.response.FeedUuidResponse;
 
 @Api
 public interface CreateFeedApi {
-    DomainFeedUuidResponse execute(DomainCreateFeedRequest request);
+    FeedUuidResponse execute(DomainCreateFeedRequest request);
 }
--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/WebFeedAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/WebFeedAdapter.java
@@ -2,9 +2,8 @@ package com.xquare.v1servicefeed.feed.web;
 
 import com.xquare.v1servicefeed.feed.api.CreateFeedApi;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
-import com.xquare.v1servicefeed.feed.api.dto.response.DomainFeedUuidResponse;
+import com.xquare.v1servicefeed.feed.api.dto.response.FeedUuidResponse;
 import com.xquare.v1servicefeed.feed.web.dto.request.WebCreateFeedRequest;
-import com.xquare.v1servicefeed.feed.web.dto.response.WebFeedUuidResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,14 +20,14 @@ public class WebFeedAdapter {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public WebFeedUuidResponse createFeed(WebCreateFeedRequest request) {
-        DomainFeedUuidResponse response = createFeedApi.execute(
+    public FeedUuidResponse createFeed(WebCreateFeedRequest request) {
+        FeedUuidResponse response = createFeedApi.execute(
                 DomainCreateFeedRequest.builder()
                         .title(request.getTitle())
                         .content(request.getContent())
                         .category(request.getCategory())
                         .build());
 
-        return new WebFeedUuidResponse(response.getFeedUuid());
+        return new FeedUuidResponse(response.getFeedUuid());
     }
 }
