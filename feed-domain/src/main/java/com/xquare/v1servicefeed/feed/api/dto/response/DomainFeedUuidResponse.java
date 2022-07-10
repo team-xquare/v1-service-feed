5d3c4b7ba15ab269d6cad45b86e6e83b3e16070a
jaemin <sonjm3518@dsm.hs.kr>
2022-07-10 18:20:52 +0900

    ⚡ :: @Builder → @AllArgsConstructor

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/dto/response/DomainFeedUuidResponse.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/dto/response/DomainFeedUuidResponse.java
@@ -1,12 +1,13 @@
 package com.xquare.v1servicefeed.feed.api.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
-@Builder
+@AllArgsConstructor
 public class DomainFeedUuidResponse {
 
     private final UUID feedUuid;
