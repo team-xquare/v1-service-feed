5335194e1671c60bd81927c70c6afc1657b62b84
KimSeungJin <klli1naver@gmail.com>
2022-07-11 11:43:11 +0900

    ⚡️ :: (#14) findById, deleteComment 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/domain/repository/CommentRepositoryAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/domain/repository/CommentRepositoryAdapter.java
@@ -2,9 +2,12 @@ package com.xquare.v1servicefeed.comment.domain.repository;
 
 import com.xquare.v1servicefeed.comment.Comment;
 import com.xquare.v1servicefeed.comment.domain.mapper.CommentMapper;
+import com.xquare.v1servicefeed.comment.exception.CommentNotFoundException;
 import com.xquare.v1servicefeed.comment.spi.CommandCommentSpi;
 import com.xquare.v1servicefeed.configuration.annotation.Adapter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Adapter
@@ -19,4 +22,22 @@ public class CommentRepositoryAdapter implements CommandCommentSpi {
                 commentMapper.domainToEntity(comment)
         );
     }
+
+    @Override
+    public Comment findById(UUID commentId) {
+        return commentMapper.entityToDomain(
+                commentRepository.findById(commentId)
+                        .orElseThrow(() -> CommentNotFoundException.EXCEPTION)
+        );
+    }
+
+    @Override
+    public void deleteComment(Comment comment) {
+        commentRepository.delete(
+                commentMapper.domainToEntity(
+                        comment
+                )
+        );
+    }
+
 }
