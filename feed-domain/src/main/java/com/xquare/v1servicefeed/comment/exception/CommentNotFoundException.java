58a03b22418719ba429448c199a6cfc0806f2caa
KimSeungJin <klli1naver@gmail.com>
2022-07-11 14:50:25 +0900

    ⚡️ :: CommentException -> FeedException

--- feed-domain/src/main/java/com/xquare/v1servicefeed/comment/exception/CommentNotFoundException.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/exception/CommentNotFoundException.java
@@ -1,13 +1,11 @@
 package com.xquare.v1servicefeed.comment.exception;
 
 import com.xquare.v1servicefeed.comment.error.CommentErrorCode;
-import com.xquare.v1servicefeed.error.CommentException;
-import com.xquare.v1servicefeed.error.ExceptionProperty;
-import com.xquare.v1servicefeed.feed.exception.FeedNotFoundException;
+import com.xquare.v1servicefeed.error.FeedException;
 
-public class CommentNotFoundException extends CommentException {
+public class CommentNotFoundException extends FeedException {
 
-    public static final CommentException EXCEPTION = new CommentNotFoundException();
+    public static final FeedException EXCEPTION = new CommentNotFoundException();
 
     public CommentNotFoundException() {
         super(CommentErrorCode.COMMENT_NOT_FOUND);
