ebb622bf4c9020e9b75cddd393cbdec34a850aeb
KimSeungJin <klli1naver@gmail.com>
2022-07-11 11:57:34 +0900

    📑 :: (#18) DeleteCommentApiImpl

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/api/impl/DeleteCommentApiImpl.java
@@ -0,0 +1,22 @@
+package com.xquare.v1servicefeed.comment.api.impl;
+
+import com.xquare.v1servicefeed.annotation.DomainService;
+import com.xquare.v1servicefeed.comment.Comment;
+import com.xquare.v1servicefeed.comment.api.DeleteCommentApi;
+import com.xquare.v1servicefeed.comment.spi.CommandCommentSpi;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@DomainService
+public class DeleteCommentApiImpl implements DeleteCommentApi {
+    private final CommandCommentSpi commandCommentSpi;
+    @Override
+    public void execute(UUID commentId) {
+        Comment comment = commandCommentSpi.findById(commentId);
+
+        commandCommentSpi.deleteComment(comment);
+    }
+    
+}
