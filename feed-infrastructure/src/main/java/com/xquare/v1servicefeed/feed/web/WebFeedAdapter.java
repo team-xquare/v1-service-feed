083fafd795099a91f9e6d0b94c43cc0fce54def8
jaemin <sonjm3518@dsm.hs.kr>
2022-07-11 15:13:47 +0900

    ⚡ :: (#17) updateFeed Controller

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/WebFeedAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/WebFeedAdapter.java
@@ -1,14 +1,16 @@
 package com.xquare.v1servicefeed.feed.web;
 
 import com.xquare.v1servicefeed.feed.api.CreateFeedApi;
+import com.xquare.v1servicefeed.feed.api.UpdateFeedApi;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
 import com.xquare.v1servicefeed.feed.web.dto.request.WebCreateFeedRequest;
+import com.xquare.v1servicefeed.feed.web.dto.request.WebUpdateFeedRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/feeds")
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebFeedAdapter {
 
     private final CreateFeedApi createFeedApi;
+    private final UpdateFeedApi updateFeedApi;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -28,5 +31,17 @@ public class WebFeedAdapter {
                 .build();
 
         createFeedApi.execute(domainRequest);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{subject-uuid}")
+    public void updateFeed(@PathVariable("subject-uuid") UUID feedId, WebUpdateFeedRequest request) {
+
+        DomainUpdateFeedRequest domainRequest = DomainUpdateFeedRequest.builder()
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+
+        updateFeedApi.execute(feedId, domainRequest);
     }
 }
