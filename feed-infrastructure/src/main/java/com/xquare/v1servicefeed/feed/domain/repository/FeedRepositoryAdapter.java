18a65137f4d14b3bffe85f1576c04fabb4545d2b
jaemin <sonjm3518@dsm.hs.kr>
2022-07-11 15:16:59 +0900

    ♻️ :: private 메소드로 분리

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/domain/repository/FeedRepositoryAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/domain/repository/FeedRepositoryAdapter.java
@@ -2,6 +2,8 @@ package com.xquare.v1servicefeed.feed.domain.repository;
 
 import com.xquare.v1servicefeed.configuration.annotation.Adapter;
 import com.xquare.v1servicefeed.feed.Feed;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
+import com.xquare.v1servicefeed.feed.domain.FeedEntity;
 import com.xquare.v1servicefeed.feed.domain.mapper.FeedMapper;
 import com.xquare.v1servicefeed.feed.exception.FeedNotFoundException;
 import com.xquare.v1servicefeed.feed.spi.FeedSpi;
@@ -23,10 +25,20 @@ public class FeedRepositoryAdapter implements FeedSpi {
     }
 
     @Override
+    public void updateFeed(UUID feedId, DomainUpdateFeedRequest request) {
+
+        FeedEntity feed = getFeedEntityById(feedId);
+
+        feed.updateFeed(request.getTitle(), request.getContent());
+    }
+
+    @Override
     public Feed queryFeedById(UUID feedId) {
-        return feedMapper.entityToDomain(
-                feedRepository.findById(feedId)
-                        .orElseThrow(() -> FeedNotFoundException.EXCEPTION)
-        );
+        return feedMapper.entityToDomain(getFeedEntityById(feedId));
+    }
+
+    private FeedEntity getFeedEntityById(UUID feedId) {
+        return feedRepository.findById(feedId)
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
     }
 }
