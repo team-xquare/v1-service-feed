069168ed7a5a4b90b627651c27057493bd1b66e9
jaemin <sonjm3518@dsm.hs.kr>
2022-07-11 15:00:03 +0900

    ⚡ :: (#17) updateFeed 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/CommandFeedSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/CommandFeedSpi.java
@@ -2,8 +2,13 @@ package com.xquare.v1servicefeed.feed.spi;
 
 import com.xquare.v1servicefeed.annotation.Spi;
 import com.xquare.v1servicefeed.feed.Feed;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
+
+import java.util.UUID;
 
 @Spi
 public interface CommandFeedSpi {
     void saveFeed(Feed feed);
+
+    void updateFeed(UUID feedId, DomainUpdateFeedRequest request);
 }
