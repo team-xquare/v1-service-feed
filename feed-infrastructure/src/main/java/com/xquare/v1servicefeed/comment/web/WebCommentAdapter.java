ebd01a5c24937919d128907cd2103fcfd8a3c759
KimSeungJin <klli1naver@gmail.com>
2022-07-11 12:02:29 +0900

    ⚡️:: (#18) deleteComment 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/WebCommentAdapter.java
@@ -1,15 +1,15 @@
 package com.xquare.v1servicefeed.comment.web;
 
 import com.xquare.v1servicefeed.comment.api.CreateCommentApi;
+import com.xquare.v1servicefeed.comment.api.DeleteCommentApi;
 import com.xquare.v1servicefeed.comment.api.dto.request.DomainCreateCommnetRequest;
 import com.xquare.v1servicefeed.comment.web.dto.request.WebCreateCommentRequest;
 import com.xquare.v1servicefeed.configuration.security.SecurityAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/comments")
@@ -19,6 +19,8 @@ public class WebCommentAdapter {
     private final SecurityAdapter securityAdapter;
 
     private final CreateCommentApi createCommentApi;
+
+    private final DeleteCommentApi deleteCommentApi;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -30,6 +32,13 @@ public class WebCommentAdapter {
                         .content(request.getContent())
                         .build()
         );
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{comment-id}")
+    public void deleteComment(@PathVariable("comment-id")UUID commentId)
+    {
+        deleteCommentApi.execute(commentId);
     }
 
 }
