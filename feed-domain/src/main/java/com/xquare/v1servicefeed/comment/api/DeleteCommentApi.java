0fc36c8c26a1dcec75ba5fb0a448321f874311b7
KimSeungJin <klli1naver@gmail.com>
2022-07-11 11:57:00 +0900

    📑 :: (#18) DeleteCommentApi

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/api/DeleteCommentApi.java
@@ -0,0 +1,10 @@
+package com.xquare.v1servicefeed.comment.api;
+
+import com.xquare.v1servicefeed.annotation.Api;
+
+import java.util.UUID;
+
+@Api
+public interface DeleteCommentApi {
+    void execute(UUID commentId);
+}
