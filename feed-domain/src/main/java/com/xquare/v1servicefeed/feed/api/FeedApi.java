d5a59a2f046d326bf766def876c5a5d33c97b5fc
KimSeungJin <klli1naver@gmail.com>
2022-09-06 23:31:50 +0900

    ♻️ :: 코드 수정

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedApi.java
@@ -1,7 +1,6 @@
 package com.xquare.v1servicefeed.feed.api;
 
 import com.xquare.v1servicefeed.annotation.Api;
-import com.xquare.v1servicefeed.comment.api.dto.request.DomainCreateCommnetRequest;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
 
