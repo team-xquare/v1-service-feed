b5c743d1e52cc6335d68bf7f0f606f112cb80cca
KimSeungJin <klli1naver@gmail.com>
2022-09-06 19:49:57 +0900

    ♻️ ::  CommentApi 아키텍처 수정

--- feed-domain/src/main/java/com/xquare/v1servicefeed/comment/api/CommentApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/api/CommentApi.java
@@ -3,7 +3,13 @@ package com.xquare.v1servicefeed.comment.api;
 import com.xquare.v1servicefeed.annotation.Api;
 import com.xquare.v1servicefeed.comment.api.dto.request.DomainCreateCommnetRequest;
 
+import java.util.UUID;
+
 @Api
-public interface CreateCommentApi {
-    void execute(DomainCreateCommnetRequest request);
+public interface CommentApi {
+    void createComment(DomainCreateCommnetRequest request);
+
+
+
+    void deleteComment(UUID commentUuid);
 }
