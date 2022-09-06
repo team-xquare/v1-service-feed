9226410a80698aec07145e747ad1d94882f3924a
KimSeungJin <klli1naver@gmail.com>
2022-09-06 19:56:35 +0900

    📑 :: DomainUpdateFeedRequest 생성

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/dto/request/DomainUpdateFeedRequest.java
@@ -0,0 +1,14 @@
+package com.xquare.v1servicefeed.feed.api.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+public class DomainUpdateFeedRequest {
+    private final UUID feedId;
+    private final String title;
+    private final String content;
+}
