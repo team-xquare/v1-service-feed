a0a93980df42489288293ee7a4cb45361ea4d74b
KimSeungJin <klli1naver@gmail.com>
2022-09-08 19:37:20 +0900

    ⚰️ :: 이상한 파일 삭제

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/UpdateFeedApi.java
+++
@@ -1,9 +0,0 @@
-package com.xquare.v1servicefeed.feed.api;
-
-import com.xquare.v1servicefeed.annotation.Api;
-import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
-
-@Api
-public interface UpdateFeedApi {
-    void execute(DomainUpdateFeedRequest request);
-}
