b407a3d687ec0af55271e5a149deb33f1caa2638
jaemin <sonjm3518@dsm.hs.kr>
2022-09-25 22:23:00 +0900

    ♻️ :: NotBlank → NotNull

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/dto/request/WebCreateCommentRequest.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/web/dto/request/WebCreateCommentRequest.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 @NoArgsConstructor
 public class WebCreateCommentRequest {
 
-    @NotBlank
+    @NotNull
     private UUID feedUuid;
 
     @NotBlank
