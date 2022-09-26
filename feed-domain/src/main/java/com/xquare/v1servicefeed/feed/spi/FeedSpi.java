3165e3d951b1661ec27dee80c661023926cbae65
jaemin <sonjm3518@dsm.hs.kr>
2022-09-26 16:46:10 +0900

    ⚡ :: QueryFeedLikeSpi 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
@@ -2,7 +2,8 @@ package com.xquare.v1servicefeed.feed.spi;
 
 import com.xquare.v1servicefeed.annotation.Spi;
 import com.xquare.v1servicefeed.comment.spi.QueryCommentSpi;
+import com.xquare.v1servicefeed.feedlike.spi.QueryFeedLikeSpi;
 
 @Spi
-public interface FeedSpi extends CommandFeedSpi, QueryCommentSpi {
+public interface FeedSpi extends CommandFeedSpi, QueryCommentSpi, QueryFeedLikeSpi {
 }
