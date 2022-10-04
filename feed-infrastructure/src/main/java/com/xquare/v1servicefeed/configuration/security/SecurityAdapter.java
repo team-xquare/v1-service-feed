107df47124e87cac3c97d574573148f496acd388
jaemin <sonjm3518@dsm.hs.kr>
2022-10-04 20:02:23 +0900

    ⚡ :: Spi Implement 받기

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/security/SecurityAdapter.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/security/SecurityAdapter.java
@@ -1,13 +1,15 @@
 package com.xquare.v1servicefeed.configuration.security;
 
 import com.xquare.v1servicefeed.configuration.annotation.Adapter;
+import com.xquare.v1servicefeed.configuration.spi.SecuritySpi;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.UUID;
 
 @Adapter
-public class SecurityAdapter {
+public class SecurityAdapter implements SecuritySpi {
 
+    @Override
     public UUID getCurrentUserId() {
         return UUID.fromString(SecurityContextHolder.getContext().getAuthentication().getName());
     }
