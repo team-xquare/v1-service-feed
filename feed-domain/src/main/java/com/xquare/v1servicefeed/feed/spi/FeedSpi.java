139c445b628c397f3690c9146214ad1f7a3fc9ea
KimSeungJin <klli1naver@gmail.com>
2022-10-18 01:01:07 +0900

    ♻️ :: 코드 리팩토링

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
@@ -4,5 +4,5 @@ import com.xquare.v1servicefeed.annotation.Spi;
 import com.xquare.v1servicefeed.comment.spi.QueryCommentSpi;
 
 @Spi
-public interface FeedSpi extends CommandFeedSpi, QueryCommentSpi, QueryFeedSpi {
+public interface FeedSpi extends CommandFeedSpi, QueryCommentSpi, QueryFeedSpi{
 }
