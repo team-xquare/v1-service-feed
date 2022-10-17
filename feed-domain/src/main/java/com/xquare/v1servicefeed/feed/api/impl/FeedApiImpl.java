d0fbb6b4bb9ab8794b512ef3c404bfef40682bce
KimSeungJin <klli1naver@gmail.com>
2022-10-18 00:22:58 +0900

    📑 :: deleteFeed 생성

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/FeedApiImpl.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/FeedApiImpl.java
@@ -1,20 +1,27 @@
 package com.xquare.v1servicefeed.feed.api.impl;
 
 import com.xquare.v1servicefeed.annotation.DomainService;
+import com.xquare.v1servicefeed.comment.spi.CommandCommentSpi;
 import com.xquare.v1servicefeed.configuration.spi.SecuritySpi;
 import com.xquare.v1servicefeed.feed.Feed;
 import com.xquare.v1servicefeed.feed.api.FeedApi;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
 import com.xquare.v1servicefeed.feed.spi.CommandFeedSpi;
+import com.xquare.v1servicefeed.feed.spi.QueryFeedSpi;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @DomainService
-public class FeedApiImpl implements FeedApi {
+public class FeedApiImpl implements FeedApi{
 
     private final CommandFeedSpi commandFeedSpi;
+    private final CommandCommentSpi commandCommentSpi;
     private final SecuritySpi securitySpi;
+    private final QueryFeedSpi queryFeedSpi;
+
 
     @Override
     public void saveFeed(DomainCreateFeedRequest request) {
@@ -31,6 +38,14 @@ public class FeedApiImpl implements FeedApi {
     @Override
     public void updateFeed(DomainUpdateFeedRequest request) {
         commandFeedSpi.updateFeed(request);
+    }
+
+    @Override
+    public void deleteFeed(UUID feedId) {
+        Feed feed = queryFeedSpi.queryFeedById(feedId);
+
+        commandFeedSpi.deleteFeed(feed);
+        commandCommentSpi.deleteAllComment(feed);
     }
 }
 
