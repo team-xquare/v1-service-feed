dfa00deb18ab2053b21411b2fc1f8e997b441da5
jaemin <sonjm3518@dsm.hs.kr>
2022-10-24 23:13:30 +0900

    ⚡ :: 에러 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feedlike/error/FeedLikeErrorCode.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feedlike/error/FeedLikeErrorCode.java
@@ -9,7 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum FeedLikeErrorCode implements ExceptionProperty {
 
-    ;
+    FEED_LIKE_NOT_FOUND(404, "Feed Like Not Found"),
+
+    INVALID_USER(401, "Invalid User");
 
     private final int status;
     private final String message;
