612ab3d4b0e171935b5a4665ba63c2b3bddf1ef3
jaemin <sonjm3518@dsm.hs.kr>
2022-10-24 23:11:27 +0900

    📑 :: QueryFeedLikeSpi

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feedlike/spi/QueryFeedLikeSpi.java
@@ -0,0 +1,14 @@
+package com.xquare.v1servicefeed.feedlike.spi;
+
+import com.xquare.v1servicefeed.annotation.Spi;
+import com.xquare.v1servicefeed.feed.Feed;
+import com.xquare.v1servicefeed.feedlike.FeedLike;
+
+import java.util.UUID;
+
+@Spi
+public interface QueryFeedLikeSpi {
+    FeedLike queryFeedLikeById(UUID feedLikeId);
+
+    FeedLike queryFeedLikeByFeed(Feed feed);
+}
