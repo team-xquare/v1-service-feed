0ea287e8f4bab2a809da76c8b8e66915cc9657bf
jaemin <sonjm3518@dsm.hs.kr>
2022-10-24 23:14:13 +0900

    ⚡ :: existsByUserId 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feedlike/domain/repository/FeedLikeRepository.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feedlike/domain/repository/FeedLikeRepository.java
@@ -1,11 +1,15 @@
 package com.xquare.v1servicefeed.feedlike.domain.repository;
 
+import com.xquare.v1servicefeed.feed.domain.FeedEntity;
 import com.xquare.v1servicefeed.feedlike.domain.FeedLikeEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FeedLikeRepository extends CrudRepository<FeedLikeEntity, UUID> {
+    Optional<FeedLikeEntity> findByFeed(FeedEntity feed);
+    boolean existsByUserId(UUID userId);
 }
