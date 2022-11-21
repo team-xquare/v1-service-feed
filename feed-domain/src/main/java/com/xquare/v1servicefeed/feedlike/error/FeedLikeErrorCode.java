ccf90856f09384a97ed8a6d919f502ea8187ddd4
jaemin <sonjm3518@dsm.hs.kr>
2022-11-21 09:24:06 +0900

    ♻️ :: status 409 → 404

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feedlike/error/FeedLikeErrorCode.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feedlike/error/FeedLikeErrorCode.java
@@ -12,7 +12,7 @@ public enum FeedLikeErrorCode implements ExceptionProperty {
     FEED_LIKE_NOT_FOUND(404, "Feed Like Not Found"),
 
     FEED_LIKE_EXISTS(409, "Feed Like Exists"),
-    FEED_LIKE_NOT_EXISTS(409, "Feed Like Not Exists");
+    FEED_LIKE_NOT_EXISTS(404, "Feed Like Not Exists");
 
     private final int status;
     private final String message;
