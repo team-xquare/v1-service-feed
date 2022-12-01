968d04207b3de0222d28b2099ee6f34729a016f9
jaemin <sonjm3518@dsm.hs.kr>
2022-12-01 14:05:44 +0900

    ⚡ :: 사용하지 않는 spi 삭제

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/FeedSpi.java
@@ -1,8 +1,7 @@
 package com.xquare.v1servicefeed.feed.spi;
 
 import com.xquare.v1servicefeed.annotation.Spi;
-import com.xquare.v1servicefeed.comment.spi.QueryCommentSpi;
 
 @Spi
-public interface FeedSpi extends CommandFeedSpi, QueryCommentSpi, QueryFeedSpi {
+public interface FeedSpi extends CommandFeedSpi, QueryFeedSpi {
 }
