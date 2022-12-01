82c03385b4176e58fd2634d2c0c00c3c210178f1
jaemin <sonjm3518@dsm.hs.kr>
2022-12-01 14:05:04 +0900

    ⚡ :: 피드Id로 댓글 찾기

--- feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/QueryCommentSpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/comment/spi/QueryCommentSpi.java
@@ -1,7 +1,13 @@
 package com.xquare.v1servicefeed.comment.spi;
 
 import com.xquare.v1servicefeed.annotation.Spi;
+import com.xquare.v1servicefeed.comment.api.dto.response.CommentDomainElement;
+
+import java.util.List;
+import java.util.UUID;
 
 @Spi
 public interface QueryCommentSpi {
+
+    List<CommentDomainElement> findCommentByFeedId(UUID feedId);
 }
