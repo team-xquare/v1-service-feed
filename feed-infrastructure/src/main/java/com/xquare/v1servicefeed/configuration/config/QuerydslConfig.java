40d44472ef15a134f41a474e9862484966df5c93
정대현 <eogus_2513@dsm.hs.kr>
2022-12-07 20:51:02 +0900

    📑 :: QuerydslConfig

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/config/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package com.xquare.v1servicefeed.configuration.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+@Configuration
+public class QuerydslConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
