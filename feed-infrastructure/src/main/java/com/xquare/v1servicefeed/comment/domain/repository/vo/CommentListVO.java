07b69eb2b6e4acb3aa8c25e8ce70c78f59246629
정대현 <eogus_2513@dsm.hs.kr>
2022-12-08 21:31:06 +0900

    📑 :: CommentListVO

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/comment/domain/repository/vo/CommentListVO.java
@@ -0,0 +1,22 @@
+package com.xquare.v1servicefeed.comment.domain.repository.vo;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class CommentListVO {
+
+    private final UUID commentId;
+    private final String content;
+    private final LocalDateTime updatedAt;
+
+    @QueryProjection
+    public CommentListVO(UUID commentId, String content, LocalDateTime updatedAt) {
+        this.commentId = commentId;
+        this.content = content;
+        this.updatedAt = updatedAt;
+    }
+}
