bcc5e91143a18c209d03b703db9d868e46030d99
jaemin <sonjm3518@dsm.hs.kr>
2022-12-27 10:11:26 +0900

    ⚡ :: 피드 이미지 추가

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/dto/request/WebUpdateFeedRequest.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/dto/request/WebUpdateFeedRequest.java
@@ -6,15 +6,16 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 public class WebUpdateFeedRequest {
 
     @NotBlank
-    @Max(255)
-    private String title;
-
-    @NotBlank
     @Max(65535)
     private String content;
+
+    @NotBlank
+    private List<String> attachmentsUrl;
 }
