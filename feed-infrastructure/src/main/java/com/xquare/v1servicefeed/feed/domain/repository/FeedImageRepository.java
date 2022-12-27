870d058d3fe3f9117c044b5ffccb586ad00e9bfe
jaemin <sonjm3518@dsm.hs.kr>
2022-12-27 10:20:11 +0900

    📑 :: FeedImageRepository

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/domain/repository/FeedImageRepository.java
@@ -0,0 +1,13 @@
+package com.xquare.v1servicefeed.feed.domain.repository;
+
+import com.xquare.v1servicefeed.feed.domain.FeedEntity;
+import com.xquare.v1servicefeed.feed.domain.FeedImageEntity;
+import com.xquare.v1servicefeed.feed.domain.FeedImageEntityId;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedImageRepository extends CrudRepository<FeedImageEntity, FeedImageEntityId> {
+
+    void deleteAllByFeed(FeedEntity feed);
+}
