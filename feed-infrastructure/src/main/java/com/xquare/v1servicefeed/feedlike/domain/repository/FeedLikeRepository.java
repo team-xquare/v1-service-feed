4be2414ada028c953c97d048ab6eaa86f501238b
jaemin <sonjm3518@dsm.hs.kr>
2022-12-28 18:56:05 +0900

    ⚡ :: FeedEntity를 이용해 FeedLikeEntity 찾기

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feedlike/domain/repository/FeedLikeRepository.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feedlike/domain/repository/FeedLikeRepository.java
@@ -1,12 +1,17 @@
 package com.xquare.v1servicefeed.feedlike.domain.repository;
 
+import com.xquare.v1servicefeed.feed.Feed;
+import com.xquare.v1servicefeed.feed.domain.FeedEntity;
 import com.xquare.v1servicefeed.feedlike.domain.FeedLikeEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FeedLikeRepository extends CrudRepository<FeedLikeEntity, UUID> {
     boolean existsByUserId(UUID userId);
+
+    Optional<FeedLikeEntity> findFeedLikeEntityByFeed(FeedEntity feedEntity);
 }
