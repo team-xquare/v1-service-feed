03b2310768d61d26f3baebf23758a1f9829dd079
jaemin <sonjm3518@dsm.hs.kr>
2022-12-29 11:33:16 +0900

    📑 :: FeedImageApiImplement

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/FeedImageApiImpl.java
@@ -0,0 +1,38 @@
+package com.xquare.v1servicefeed.feed.api.impl;
+
+import com.xquare.v1servicefeed.annotation.DomainService;
+import com.xquare.v1servicefeed.feed.FeedImage;
+import com.xquare.v1servicefeed.feed.api.FeedImageApi;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedImageRequest;
+import com.xquare.v1servicefeed.feed.spi.CommandFeedImageSpi;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@DomainService
+public class FeedImageApiImpl implements FeedImageApi {
+
+    private final CommandFeedImageSpi commandFeedImageSpi;
+
+    @Override
+    public void saveAllFeedImage(DomainCreateFeedImageRequest request) {
+        List<FeedImage> feedImageList = new ArrayList<>();
+        for (int i = 0; i < request.getAttachments_url().size(); i++) {
+            FeedImage feedImage = FeedImage.builder()
+                    .feedId(request.getFeedId())
+                    .filePath(request.getAttachments_url().get(i))
+                    .number(i)
+                    .build();
+            feedImageList.add(feedImage);
+        }
+        commandFeedImageSpi.saveAllFeedImage(feedImageList);
+    }
+
+    @Override
+    public void deleteAllFeedImage(UUID feedId) {
+        commandFeedImageSpi.deleteAllFeedImage(feedId);
+    }
+}
