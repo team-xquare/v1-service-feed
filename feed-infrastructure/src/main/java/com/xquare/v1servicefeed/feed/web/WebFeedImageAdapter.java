ddfb4118367d6ee3fd5bdeca91dc1a08ddb7c8b1
jaemin <sonjm3518@dsm.hs.kr>
2022-12-29 11:34:25 +0900

    📑 :: WebFeedImageAdapter

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/feed/web/WebFeedImageAdapter.java
@@ -0,0 +1,35 @@
+package com.xquare.v1servicefeed.feed.web;
+
+import com.xquare.v1servicefeed.feed.api.FeedImageApi;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedImageRequest;
+import com.xquare.v1servicefeed.feed.web.dto.request.WebCreateFeedImageRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RequestMapping("/images")
+@RestController
+public class WebFeedImageAdapter {
+
+    private final FeedImageApi feedImageApi;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-uuid}")
+    public void saveFeedImage(@PathVariable("feed-uuid") UUID feedId, @Valid @RequestBody WebCreateFeedImageRequest request) {
+        DomainCreateFeedImageRequest domainCreateFeedImageRequest = DomainCreateFeedImageRequest.builder()
+                .feedId(feedId)
+                .attachments_url(request.getAttachments_url())
+                .build();
+        feedImageApi.saveAllFeedImage(domainCreateFeedImageRequest);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feed-uuid}")
+    public void deleteAllFeedImage(@PathVariable("feed-uuid") UUID feedId) {
+        feedImageApi.deleteAllFeedImage(feedId);
+    }
+}
