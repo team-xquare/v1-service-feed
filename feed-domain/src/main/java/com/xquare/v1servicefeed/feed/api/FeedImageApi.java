92054915c3679d952a13e0744037b931c4c410bf
정대현 <eogus_2513@dsm.hs.kr>
2022-12-31 00:08:24 +0900

    ⚡️ :: 피드 이미지 업데이트 API 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedImageApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedImageApi.java
@@ -2,6 +2,7 @@ package com.xquare.v1servicefeed.feed.api;
 
 import com.xquare.v1servicefeed.annotation.Api;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedImageRequest;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedImageRequest;
 
 import java.util.UUID;
 
@@ -11,4 +12,6 @@ public interface FeedImageApi {
     void saveAllFeedImage(DomainCreateFeedImageRequest request);
 
     void deleteAllFeedImage(UUID feedId);
+
+    void updateFeedImage(DomainUpdateFeedImageRequest request);
 }
--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/FeedImageApiImpl.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/impl/FeedImageApiImpl.java
@@ -1,10 +1,14 @@
 package com.xquare.v1servicefeed.feed.api.impl;
 
 import com.xquare.v1servicefeed.annotation.DomainService;
+import com.xquare.v1servicefeed.feed.Feed;
 import com.xquare.v1servicefeed.feed.FeedImage;
 import com.xquare.v1servicefeed.feed.api.FeedImageApi;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedImageRequest;
+import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedImageRequest;
 import com.xquare.v1servicefeed.feed.spi.CommandFeedImageSpi;
+import com.xquare.v1servicefeed.feed.spi.QueryFeedImageSpi;
+import com.xquare.v1servicefeed.feed.spi.QueryFeedSpi;
 import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
@@ -16,6 +20,8 @@ import java.util.UUID;
 public class FeedImageApiImpl implements FeedImageApi {
 
     private final CommandFeedImageSpi commandFeedImageSpi;
+    private final QueryFeedImageSpi queryFeedImageSpi;
+    private final QueryFeedSpi queryFeedSpi;
 
     @Override
     public void saveAllFeedImage(DomainCreateFeedImageRequest request) {
@@ -34,5 +40,48 @@ public class FeedImageApiImpl implements FeedImageApi {
     @Override
     public void deleteAllFeedImage(UUID feedId) {
         commandFeedImageSpi.deleteAllFeedImage(feedId);
+    }
+
+    @Override
+    public void updateFeedImage(DomainUpdateFeedImageRequest request) {
+        Feed feed = queryFeedSpi.queryFeedById(request.getFeedId());
+        List<FeedImage> feedImageList = queryFeedImageSpi.queryAllByFeed(feed);
+
+        if (request.getAttachmentsUrl().size() > feedImageList.size()) {
+            for (int i = 0; i < request.getAttachmentsUrl().size(); i++) {
+                if (i < feedImageList.size()) {
+                    if (!request.getAttachmentsUrl().get(i).equals(feedImageList.get(i).getFilePath())) {
+                        saveFeedImage(i, request.getFeedId(), request.getAttachmentsUrl().get(i));
+                    }
+                } else {
+                    saveFeedImage(i, request.getFeedId(), request.getAttachmentsUrl().get(i));
+                }
+            }
+        } else if (feedImageList.size() > request.getAttachmentsUrl().size()) {
+            for (int i = 0; i < feedImageList.size(); i++) {
+                if (i < request.getAttachmentsUrl().size()) {
+                    if (!request.getAttachmentsUrl().get(i).equals(feedImageList.get(i).getFilePath())) {
+                        saveFeedImage(i, request.getFeedId(), request.getAttachmentsUrl().get(i));
+                    }
+                } else {
+                    commandFeedImageSpi.deleteFeedImage(feedImageList.get(i));
+                }
+            }
+        } else {
+            for (int i = 0; i < request.getAttachmentsUrl().size(); i++) {
+                if (!request.getAttachmentsUrl().get(i).equals(feedImageList.get(i).getFilePath())) {
+                    saveFeedImage(i, request.getFeedId(), request.getAttachmentsUrl().get(i));
+                }
+            }
+        }
+    }
+
+    private void saveFeedImage(Integer number, UUID feedId, String filePath) {
+        FeedImage feedImage = FeedImage.builder()
+                .number(number)
+                .feedId(feedId)
+                .filePath(filePath)
+                .build();
+        commandFeedImageSpi.saveFeedImage(feedImage);
     }
 }
