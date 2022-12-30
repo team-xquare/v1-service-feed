937a4ca9717b014ead8f836bcb376de9c3e70646
정대현 <eogus_2513@dsm.hs.kr>
2022-12-31 01:41:43 +0900

    📑 :: QueryCategorySpi

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/QueryCategorySpi.java
@@ -0,0 +1,10 @@
+package com.xquare.v1servicefeed.feed.spi;
+
+import com.xquare.v1servicefeed.feed.Category;
+
+import java.util.List;
+
+public interface QueryCategorySpi {
+
+    List<Category> queryAllCategory();
+}
