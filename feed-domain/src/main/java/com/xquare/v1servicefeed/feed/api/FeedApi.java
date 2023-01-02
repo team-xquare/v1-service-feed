6f060dc67586ad050d8644870388da79d64557a5
KimSeungJin <klli1naver@gmail.com>
2023-01-02 10:54:00 +0900

    📑 :: saveFeed 반환 수정

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedApi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/api/FeedApi.java
@@ -5,12 +5,13 @@ import com.xquare.v1servicefeed.feed.api.dto.request.DomainCreateFeedRequest;
 import com.xquare.v1servicefeed.feed.api.dto.request.DomainUpdateFeedRequest;
 import com.xquare.v1servicefeed.feed.api.dto.response.FeedCategoryResponse;
 import com.xquare.v1servicefeed.feed.api.dto.response.FeedListResponse;
+import com.xquare.v1servicefeed.feed.api.dto.response.FeedSaveResponse;
 
 import java.util.UUID;
 
 @Api
 public interface FeedApi {
-    void saveFeed(DomainCreateFeedRequest request);
+    FeedSaveResponse saveFeed(DomainCreateFeedRequest request);
 
     void updateFeed(DomainUpdateFeedRequest request);
 
