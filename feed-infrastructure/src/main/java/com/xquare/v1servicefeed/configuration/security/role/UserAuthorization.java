13bef81f146edbe21aa8f9c88f658eb15fdd2f63
KimSeungJin <klli1naver@gmail.com>
2023-01-02 10:45:01 +0900

    📑 :: Authorization enum 추가

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/configuration/security/role/UserAuthorization.java
@@ -0,0 +1,11 @@
+package com.xquare.v1servicefeed.configuration.security.role;
+
+public enum UserAuthorization {
+    STD, //student department
+    STC, // student council
+    STA, //student' autonomy
+    DOD, //dormitory department
+    DOS, //dormitory supervisor
+    CLL // club leader
+}
+
