f678b060c0af6b69a3807cc35bd60eae1ba2fce4
jaemin <sonjm3518@dsm.hs.kr>
2023-01-04 20:44:04 +0900

    ⚡ :: 일반 학생 권한 추가

--- feed-domain/src/main/java/com/xquare/v1servicefeed/user/role/UserAuthority.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/user/role/UserAuthority.java
@@ -1,11 +1,12 @@
 package com.xquare.v1servicefeed.user.role;
 
-public enum UserAuthorization {
+public enum UserAuthority {
     STD, //student department
-    STC, // student council
-    STA, //student' autonomy
+    STC, //student council
+    STA, //student autonomy
     DOD, //dormitory department
     DOS, //dormitory supervisor
-    CLL // club leader
+    CLL, // club leader
+    OST, // ordinary student
 }
 
