6ff23b77f9943b53778bd339060acce76dd7870a
jaemin <sonjm3518@dsm.hs.kr>
2023-01-04 20:46:22 +0900

    ⚡ :: Id로 카테고리 조회

--- feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/QueryCategorySpi.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/feed/spi/QueryCategorySpi.java
@@ -3,8 +3,11 @@ package com.xquare.v1servicefeed.feed.spi;
 import com.xquare.v1servicefeed.feed.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface QueryCategorySpi {
+
+    Category queryCategoryById(UUID categoryId);
 
     List<Category> queryAllCategory();
 }
