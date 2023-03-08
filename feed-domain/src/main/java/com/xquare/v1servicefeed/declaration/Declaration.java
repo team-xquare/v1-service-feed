130dad2f4e95430647099eae45f2e94a2a68d501
min56 <minchaeg85@gmail.com>
2023-03-08 23:24:05 +0900

    📑 :: Declaration

---
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/declaration/Declaration.java
@@ -0,0 +1,21 @@
+package com.xquare.v1servicefeed.declaration;
+
+import com.xquare.v1servicefeed.annotation.Aggregate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@Aggregate
+public class Declaration {
+    private final UUID id;
+
+    private final String content;
+
+    private final UUID feedId;
+
+    private final UUID userId;
+
+}
