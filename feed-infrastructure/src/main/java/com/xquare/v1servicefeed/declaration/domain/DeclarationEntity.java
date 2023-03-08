a5a1fa18e903284488d3a74d353e6e0a3661b548
min56 <minchaeg85@gmail.com>
2023-03-08 23:22:15 +0900

    📑 :: DeclarationEntity

---
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/declaration/domain/DeclarationEntity.java
@@ -0,0 +1,31 @@
+package com.xquare.v1servicefeed.declaration.domain;
+
+import com.xquare.v1servicefeed.configuration.entity.BaseUUIDEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_declaration")
+@Entity
+public class DeclarationEntity extends BaseUUIDEntity {
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private UUID feedId;
+
+    @Column(nullable = false)
+    private String content;
+}
