715056f7c1c5d30841dc93132e2f300fa1303923
min56 <minchaeg85@gmail.com>
2023-03-09 16:28:07 +0900

    ♻️ :: 코드 정렬

--- feed-infrastructure/src/main/java/com/xquare/v1servicefeed/report/domain/mapper/ReportMapperImpl.java
+++ feed-infrastructure/src/main/java/com/xquare/v1servicefeed/report/domain/mapper/ReportMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class ReportMapperImpl implements ReportMapper{
+public class ReportMapperImpl implements ReportMapper {
     public Report entityToDomain(ReportEntity reportEntity) {
 
         return Report.builder()
