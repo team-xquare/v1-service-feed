36a5edb4d5871cda96760a898849fda164037e2b
min56 <minchaeg85@gmail.com>
2023-05-31 18:42:02 +0900

    ♻️ :: 자신의 피드 신고시 예외처리

--- feed-domain/src/main/java/com/xquare/v1servicefeed/report/api/impl/ReportApiImpl.java
+++ feed-domain/src/main/java/com/xquare/v1servicefeed/report/api/impl/ReportApiImpl.java
@@ -5,10 +5,13 @@ import com.xquare.v1servicefeed.configuration.spi.SecuritySpi;
 import com.xquare.v1servicefeed.report.Report;
 import com.xquare.v1servicefeed.report.api.ReportApi;
 import com.xquare.v1servicefeed.report.api.dto.CreateReportDomainRequest;
+import com.xquare.v1servicefeed.report.exception.CannotReportMyFeedException;
 import com.xquare.v1servicefeed.report.spi.CommandReportSpi;
 import com.xquare.v1servicefeed.feed.Feed;
 import com.xquare.v1servicefeed.feed.spi.QueryFeedSpi;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @DomainService
@@ -21,10 +24,15 @@ public class ReportApiImpl implements ReportApi {
     @Override
     public void saveReport(CreateReportDomainRequest request) {
         Feed feed = queryFeedSpi.queryFeedById(request.getFeedId());
+        UUID userId = securitySpi.getCurrentUserId();
+
+        if(feed.getUserId().equals(feed.getUserId())) {
+            throw CannotReportMyFeedException.EXCEPTION;
+        }
 
         commandReportSpi.saveReport(
                 Report.builder()
-                        .userId(securitySpi.getCurrentUserId())
+                        .userId(userId)
                         .reportUserId(feed.getUserId())
                         .feedId(feed.getId())
                         .content(request.getContent())
